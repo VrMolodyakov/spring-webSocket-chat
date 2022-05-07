1431a62ff3e089ef36ec0410bf0e9b68e7e97f2d
Vyacheslav Molodyakov <vrmolodyakov@mail.ru>
2022-05-08 02:27:32 +0300

    chatting

--- src/main/java/com/example/webSocketChat/Controller/MessageController.java
+++ src/main/java/com/example/webSocketChat/Controller/MessageController.java
@@ -3,6 +3,7 @@ package com.example.webSocketChat.Controller;
 import com.example.webSocketChat.Messages.Message;
 import com.example.webSocketChat.Messages.Name;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
@@ -11,7 +12,7 @@ public class MessageController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/chatting")
-    public Message messageHandler(Name name) throws Exception{
+    public Message messageHandler(@Payload Name name) throws Exception{
         return new Message("Hello, " + name.getName() + " !");
     }
 }
